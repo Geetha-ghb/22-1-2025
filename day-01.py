293cd591a1b3cfb740dc37fea8c4fcd18619dd7e
Geetha-ghb <guggillageethanjali@gmail.com>
2025-02-04 19:05:55 +0530

    Add files via upload

---
+++ day-01.py
@@ -0,0 +1,65 @@
+# print('Arithematic operations')
+# num1= 21
+# num2=24
+# print(num1+ num2)
+# print(num1- num2)
+# #if num 2 value is changed to num2=56 then
+# num2=56
+# print(num1* num2)
+# print(num1/ num2)
+# print(num1// num2)
+# #strings
+# str1= "em antivi em antivi....."
+# print(len(str1))
+# print(str1[4])
+# print(str1[0: 3])
+# #To calculate the powers
+# num=6
+# print(num**2)
+# #To print string
+# print("Em antivi em antivi...")
+
+
+print("ARITHMETIC OPERATIONS")
+print(8+5)
+print(8-5)
+print(8*5)
+print(8/5)
+print(8//5)
+#Exponent
+print(5**8)
+#variables
+a = 85
+b = 58
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+#Changing "b" value
+b = 35
+c = 94.93
+print(type(b))
+print(type(c))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+pooji = 1234
+print(pooji)
+print("Geetha")
+str = "Geetha @123"
+print(len(str))
+#indexing
+print(str[0])
+print(str[5])
+print(str[-1])
+print(str[-4])
+#slicing operation
+print(str[0:5])
+print(str[6:11])
+#step index
+print(str[0::2])
+
+
